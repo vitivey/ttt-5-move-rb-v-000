ed3ab68db7248900789c3bb3a19b2442a00f4175
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-07-31 17:35:41 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -14,6 +14,6 @@ def input_to_index(input)
   index = input.to_i - 1
 end
 
-def move(board, index, character = "X")
-  board[index] = character
+def move(array, index, character = "X")
+  array[index] = character
 end
